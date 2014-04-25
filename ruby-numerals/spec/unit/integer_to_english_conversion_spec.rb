67c2dae4e52cf8c1a98e6d554249f849a7dfa355
Juan Lebrijo <juan@lebrijo.com>
2014-04-25 17:49:59 +0200

    First spec pending

---
+++ ruby-numerals/spec/unit/integer_to_english_conversion_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Integer to English conversion" do
+  it "converts to zero"
+  it "converts 7 to seven"
+  it "converts 42 to forty-two"
+  it "converts 1999 to nineteen hundred and ninety-nine"
+  it "converts 2001 to two thousand and one"
+end
