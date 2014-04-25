38be2de5276a43adfc9fbe85e67df82db2d0991a
Juan Lebrijo <juan@lebrijo.com>
2014-04-25 17:58:15 +0200

    0 - 19 pass refactored to an Array

--- ruby-numerals/lib/integer.rb
+++ ruby-numerals/lib/integer.rb
@@ -1,5 +1,10 @@
 class Integer
+
+  BASIC_NUMBERS = %w( zero one two three four five six seven eight nine ten
+                        eleven twelve thirteen fourteen fifteen sixteen
+                        seventeen eighteen nineteen )
+
   def to_english
-    return "zero"
+    return BASIC_NUMBERS[self]
   end
 end
--- ruby-numerals/spec/unit/integer_to_english_conversion_spec.rb
+++ ruby-numerals/spec/unit/integer_to_english_conversion_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
+require_relative '../../lib/integer'
 
 describe "Integer to English conversion" do
-  it "converts to zero"
-  it "converts 7 to seven"
+  it "converts 0 to zero"do
+    0.to_english.should == "zero"
+  end
+  it "converts 1 to one"do
+    1.to_english.should == "one"
+  end
+  it "converts 7 to seven"do
+    7.to_english.should == "seven"
+  end
+  it "converts 19 to nineteen"do
+    19.to_english.should == "nineteen"
+  end
   it "converts 42 to forty-two"
   it "converts 1999 to nineteen hundred and ninety-nine"
   it "converts 2001 to two thousand and one"
