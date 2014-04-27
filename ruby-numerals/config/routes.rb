bf462e60635cb6626f3a03c9c97f7eeaea4394f6
Juan Lebrijo <juan@lebrijo.com>
2014-04-27 11:23:08 +0200

    Actualizing Rails views to vigintillions

--- ruby-numerals/config/routes.rb
+++ ruby-numerals/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root to: "convert#to_english"
   get "to_english", to: 'convert#to_english'
   post "to_english", to: 'convert#to_english'
 
--- ruby-numerals/spec/features/convert_to_english_spec.rb
+++ ruby-numerals/spec/features/convert_to_english_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature "Integer to English conversion" do
   scenario "/to_english converts a number into an english phrase" do
-    visit to_english_path
+    visit root_path
     fill_in "Number", with: 184304
     click_on "Convert"
 
